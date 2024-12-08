7cd0a83ec6d91e1dd75218b4ab82499061cfcb2a
tosu <timo42@proton.me>
2024-12-08 02:24:55 +0100

    ex00

---
+++ ex00/ClapTrap.hpp
@@ -0,0 +1,43 @@
+#pragma once /* ClapTrap.hpp */
+
+#include <string> /* std::string */
+#include <iostream> /* std::ostream */
+
+using std::string;
+using std::ostream;
+
+class ClapTrap {
+public:
+	~ClapTrap();
+	ClapTrap();
+	ClapTrap(string name);
+	ClapTrap(string name, unsigned int hit_points, unsigned int energy_points, unsigned int attack_damage);
+	ClapTrap(const ClapTrap&);
+	ClapTrap& operator=(ClapTrap);
+	void swap(ClapTrap&);
+	string repr() const;
+	operator string() const;
+
+	const string& name() const;
+	const unsigned int& hit_points() const;
+	const unsigned int& energy_points() const;
+	const unsigned int& attack_damage() const;
+
+	void name(const std::string&);
+	void hit_points(const unsigned int&);
+	void energy_points(const unsigned int&);
+	void attack_damage(const unsigned int&);
+
+	void attack(const string& target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+private:
+	string _name;
+	unsigned int _hit_points;
+	unsigned int _energy_points;
+	unsigned int _attack_damage;
+};
+
+void swap(ClapTrap& a, ClapTrap& b) /* noexcept */;
+ostream& operator<<(ostream&, const ClapTrap&);
+string repr(const ClapTrap& value);
