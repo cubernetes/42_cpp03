7a375e9c8d056ade309afc31a266843104dcd94c
cubernetes <timo42@proton.me>
2024-12-18 10:46:37 +0100

    Fix bug, add debugging

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,13 +3,20 @@
 #include "ClapTrap.hpp"
 
 int main() {
+	std::cout << "\x1b\x5b""31mTesting CONSTRUCTOR\x1b\x5bm\n";
 	ClapTrap bob("Bob");
-	trap.attack("Sara");
+	std::cout << "\x1b\x5b""31mTesting ATTACK\x1b\x5bm\n";
+	bob.attack("Sara");
+	std::cout << "\x1b\x5b""31mTesting TAKE DAMAGE\x1b\x5bm\n";
 	for (int i = 0; i < 2; ++i)
-		trap.takeDamage(3);
-	trap.beRepaired(10);
+		bob.takeDamage(3);
+	std::cout << "\x1b\x5b""31mTesting REPARING\x1b\x5bm\n";
+	bob.beRepaired(10);
+	std::cout << "\x1b\x5b""31mTesting TAKE DAMAGE\x1b\x5bm\n";
 	for (int i = 0; i < 7; ++i)
-		trap.takeDamage(3);
-	trap.beRepaired(10);
+		bob.takeDamage(3);
+	std::cout << "\x1b\x5b""31mTesting REPARING\x1b\x5bm\n";
+	bob.beRepaired(10);
+	std::cout << "\x1b\x5b""31mTesting DESTRUCTOR\x1b\x5bm\n";
 	return 0;
 }
